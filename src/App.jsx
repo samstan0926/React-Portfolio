27eed7996e6c5582e31ad2c19786d5acbc4763e0
samstan092 <samstan092@gmail.com>
2024-12-09 21:21:37 -0600

    ee

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import AboutMe from './components/AboutMe';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
+import { BrowserRouter } from 'react-router-dom'
 function App() {
     return (
         <>
+        <BrowserRouter>
             <AboutMe />
             <Outlet />
+        </BrowserRouter>
 
         </>
     );
