a2f949135f53ec07d52046643872ea3f2eb338b9
samstan092 <samstan092@gmail.com>
2024-12-09 22:48:19 -0600

    E

--- demo/src/App.jsx
+++ demo/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer.jsx';
-import Header from './components/header.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
     return (
